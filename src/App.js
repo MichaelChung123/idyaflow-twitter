93c5ea19c42f8ee46a8683fd145ab8c23ad5fe6c
MichaelChung123 <michael.chung1994@gmail.com>
2019-03-11 18:09:22 -0700

    setting up passport

--- src/App.js
+++ src/App.js
@@ -2,7 +2,35 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import io from 'socket.io-client'
+import OAuth from './OAuth'
+import { API_URL } from './config'
+const socket = io(API_URL)
+const providers = ['twitter', 'google', 'facebook', 'github']
+
 class App extends Component {
+
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     name: '',
+  //     greeting: ''
+  //   };
+  //   this.handleChange = this.handleChange.bind(this);
+  //   this.handleSubmit = this.handleSubmit.bind(this);
+  // }
+
+  // handleChange(event) {
+  //   this.setState({ name: event.target.value });
+  // }
+
+  // handleSubmit(event) {
+  //   event.preventDefault();
+  //   fetch(`/api/greeting?name=${encodeURIComponent(this.state.name)}`)
+  //     .then(response => response.json())
+  //     .then(state => this.setState(state));
+  // }
+
   render() {
     return (
       <div className="App">
@@ -11,6 +39,32 @@ class App extends Component {
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
+
+          {/* <form onSubmit={this.handleSubmit}>
+            <label htmlFor="name">Enter your name: </label>
+            <input
+              id="name"
+              type="text"
+              value={this.state.name}
+              onChange={this.handleChange}
+            />
+            <button type="submit">Submit</button>
+          </form>
+
+          <p>{this.state.greeting}</p> */}
+
+          <div className={'wrapper'}>
+            <div className={'container'}>
+              {providers.map(provider =>
+                <OAuth
+                  provider={provider}
+                  key={provider}
+                  socket={socket}
+                />
+              )}
+            </div>
+          </div>
+
           <a
             className="App-link"
             href="https://reactjs.org"
